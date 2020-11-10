8aa38fa73997bc0f823ddf78d95ba7512aec3017
Matias-Pavon <matiaspavona@gmail.com>
2020-11-10 00:41:07 -0400

    Cambios

---
+++ principal.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Matias Hector Pavon" << endl;
+    cout << "21 de julio del 2000" << endl;
+    cout << "Buenos Aires-Quilmes" << endl;
+    cout << "20 años" << endl;
+    cout << "Estudiante Universitario" << endl;
+}
